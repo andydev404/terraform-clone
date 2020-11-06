e12c1f03fa475b812533b532c8a1d53fd9a9e37b
Andy Santana <andysantana0106@gmail.com>
2020-11-06 16:16:18 -0400

    Terraform clone

---
+++ README.md
@@ -0,0 +1,5 @@
+## Terraform Clone with [Tailwind](https://tailwindcss.com)
+
+![alt text](https://i.ibb.co/0Cbckgn/Screen-Shot-2020-11-06-at-4-10-46-PM.png "Terraform clone")
+
+[DEMO](https://terraform-clone.netlify.app)
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'purple-light': '#623ce4',
+        'purple-dark': '#3c2aa8',
+        'gray-light': '#f7f8fa',
+        'gray-dark': '#eaeaeb'
+      },
+      borderColor: {
+        'purple-light': '#623ce4',
+        'purple-dark': '#3c2aa8'
+      },
+      textColor: {
+        'purple-light': '#623ce4',
+        'purple-dark': '#3c2aa8'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
